cbe573878984ad47d78020df3617f545ccf34658
Takuya Kikuchi <kikuchi.takuya.dbce8@gmail.com>
2020-04-20 19:04:09 +0900

    done 17

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 var person = {
     name: 'TK',
-    age: 30
+    age: 30,
+    hobbies: ['soccer', 'coding']
 };
 console.log(person.name, person.age);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toUpperCase());
+}
--- app.ts
+++ app.ts
@@ -1,9 +1,10 @@
-const person: {
-  name: string
-  age: number
-} = {
+const person = {
   name: 'TK',
-  age:30
+  age: 30,
+  hobbies: ['soccer', 'coding']
 }
 
 console.log(person.name, person.age)
+for (const hobby of person.hobbies) {
+  console.log(hobby.toUpperCase())
+}
