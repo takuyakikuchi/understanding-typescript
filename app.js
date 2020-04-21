e6d7d1ff031b813c674487dadb3a5c9fab32e3b6
Takuya Kikuchi <kikuchi.takuya.dbce8@gmail.com>
2020-04-21 09:22:29 +0900

    19 done

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
+var Role;
+(function (Role) {
+    Role["ADMIN"] = "admin";
+    Role["READ_ONLY"] = "read_only";
+    Role["AUTHOR"] = "author";
+})(Role || (Role = {}));
 var person = {
     name: 'TK',
     age: 30,
-    hobbies: ['soccer', 'coding']
+    hobbies: ['soccer', 'coding'],
+    role: Role.ADMIN
 };
 console.log(person.name, person.age);
 for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
     var hobby = _a[_i];
     console.log(hobby.toUpperCase());
 }
+console.log(person.role);
--- app.ts
+++ app.ts
@@ -1,10 +1,14 @@
+enum Role {ADMIN = 'admin', READ_ONLY = 'read_only', AUTHOR = 'author'}
+
 const person = {
   name: 'TK',
   age: 30,
-  hobbies: ['soccer', 'coding']
+  hobbies: ['soccer', 'coding'],
+  role: Role.ADMIN
 }
 
 console.log(person.name, person.age)
 for (const hobby of person.hobbies) {
   console.log(hobby.toUpperCase())
 }
+console.log(person.role)
