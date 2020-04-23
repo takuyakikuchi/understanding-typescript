e15d532f431d39d08cf747e1d5c47c1875fafd06
Takuya Kikuchi <kikuchi.takuya.dbce8@gmail.com>
2020-04-23 09:52:03 +0900

    39:Change tsconfig file

--- app.js
+++ app.js
@@ -2,11 +2,11 @@
 function add(input1, input2) {
     return input1 + input2;
 }
-var combineValues;
+let combineValues;
 combineValues = add;
 console.log(combineValues(1, 3));
 function addAndHandle(input1, input2, cb) {
-    var result = input1 + input2;
+    const result = input1 + input2;
     cb(result);
 }
-console.log(addAndHandle(3, 3, function () { }));
+console.log(addAndHandle(3, 3, () => { }));
--- basics.ts
+++
@@ -1,9 +0,0 @@
-function add(num1: number, num2: number) {
-  return num1 + num2
-}
-
-const x = 5
-const y = 2.5
-
-const result = add(x, y)
-console.log(result)
