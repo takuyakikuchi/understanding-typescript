d57365a4007b8be4ae56edd20f620bf4fd02ec18
Takuya Kikuchi <kikuchi.takuya.dbce8@gmail.com>
2020-05-02 11:54:03 +0900

    Generic Class

--- dist/app.js
+++ dist/app.js
@@ -24,4 +24,33 @@ function extractWithKey(obj, key) {
 }
 // console.log(extractWithKey({ japan: 'Japanese' }, 'japan'));
 // console.log(extractWithKey({ japan: 'Japanese' }, 'us')); // Error
+// --------- Generic Class -----------
+class DataStorage {
+    constructor() {
+        this.data = [];
+    }
+    addData(element) {
+        this.data.push(element);
+    }
+    removeData(element) {
+        this.data.splice(this.data.indexOf(element), 1);
+    }
+    getData() {
+        return [...this.data];
+    }
+}
+// String test
+// const stringData = new DataStorage<string>();
+// stringData.addData('Takuya');
+// stringData.addData('Fumiko');
+// console.log(`Before remove ${stringData.getData()}`);
+// stringData.removeData('Fumiko');
+// console.log(`After remove ${stringData.getData()}`);
+// Number test
+// const numberData = new DataStorage<number>();
+// numberData.addData(1);
+// numberData.addData(2);
+// console.log(`Before remove ${numberData.getData()}`);
+// numberData.removeData(1);
+// console.log(`After remove ${numberData.getData()}`);
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -39,3 +39,36 @@ function extractWithKey<T extends object, U extends keyof T>(obj: T, key: U) {
 
 // console.log(extractWithKey({ japan: 'Japanese' }, 'japan'));
 // console.log(extractWithKey({ japan: 'Japanese' }, 'us')); // Error
+
+// --------- Generic Class -----------
+class DataStorage<T> {
+  data: T[] = [];
+
+  addData(element: T) {
+    this.data.push(element);
+  }
+
+  removeData(element: T) {
+    this.data.splice(this.data.indexOf(element), 1);
+  }
+
+  getData() {
+    return [...this.data];
+  }
+}
+
+// String test
+// const stringData = new DataStorage<string>();
+// stringData.addData('Takuya');
+// stringData.addData('Fumiko');
+// console.log(`Before remove ${stringData.getData()}`);
+// stringData.removeData('Fumiko');
+// console.log(`After remove ${stringData.getData()}`);
+
+// Number test
+// const numberData = new DataStorage<number>();
+// numberData.addData(1);
+// numberData.addData(2);
+// console.log(`Before remove ${numberData.getData()}`);
+// numberData.removeData(1);
+// console.log(`After remove ${numberData.getData()}`);
