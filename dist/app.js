f47aba3cbc76bd22a2de344aa84d39f34e5a6f64
Takuya Kikuchi <kikuchi.takuya.dbce8@gmail.com>
2020-05-05 12:18:06 +0900

    Accessor&Parameter decorator

--- dist/app.js
+++ dist/app.js
@@ -1,43 +1,105 @@
 "use strict";
+/** @format */
+/*
+function Logger(randomString: string) {
+  console.log('Logger factory');
+  return function (constructor: Function) {
+    console.log(randomString);
+    console.log(constructor);
+  };
+}
+
+function WithTemplate(template: string, id: string) {
+  console.log('Template factory');
+  return function (constructor: Function) {
+    console.log('Rendering template');
+    const appDiv = document.getElementById(id);
+    const newPerson = new Person();
+    if (appDiv) {
+      appDiv.innerHTML = template;
+      appDiv.querySelector('h1')!.textContent = newPerson.name
+    }
+  }
+}
+
+/* ---------- Decorators ----------
+ *<Description>
+ *Decorators provide a way to add both annotations and a meta-programming syntax for class *declarations and members
+ *<Timing of execution>
+ *It is executed when class exists, not when class initiated
+ */
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-/** @format */
-function Logger(randomString) {
-    console.log('Logger factory');
-    return function (constructor) {
-        console.log(randomString);
-        console.log(constructor);
-    };
-}
-function WithTemplate(template, id) {
-    console.log('Template factory');
-    return function (constructor) {
-        console.log('Rendering template');
-        const appDiv = document.getElementById(id);
-        const newPerson = new Person();
-        if (appDiv) {
-            appDiv.innerHTML = template;
-            appDiv.querySelector('h1').textContent = newPerson.name;
-        }
-    };
-}
-/* ---------- Decorators ----------
- *Decorators provide a way to add both annotations and a meta-programming syntax for class *declarations and members
- */
-let Person = class Person {
-    constructor() {
-        this.name = 'Takuya Kikuchi';
-        console.log('Creating new person...');
-    }
+var __param = (this && this.__param) || function (paramIndex, decorator) {
+    return function (target, key) { decorator(target, key, paramIndex); }
 };
-Person = __decorate([
-    Logger('Logging'),
-    WithTemplate('<h1>My Person Object</h1>', 'app')
-], Person);
+/*
+@Logger('Logging')
+@WithTemplate('<h1>My Person Object</h1>', 'app')
+class Person {
+  name = 'Takuya Kikuchi';
+
+  constructor() {
+    console.log('Creating new person...');
+  }
+}
+
 const takuya = new Person();
 console.log(takuya);
+*/
+//  --------------- Accessor & Parameter Decorator -----------
+function PropertyDecorator(target, propertyName) {
+    console.log('------ Property decorator --------');
+    console.log(`target: ${target}`);
+    console.log(`propertyName: ${propertyName}`);
+}
+function AccessorDecorator(target, name, descriptor) {
+    console.log('------ Accessor decorator ------');
+    console.log(`target: ${target}`);
+    console.log(`name: ${name}`);
+    console.log(`descriptor: ${descriptor}`);
+}
+function MethodDecorator(target, name, descriptor) {
+    console.log('------ Method decorator ------');
+    console.log(`target: ${target}`);
+    console.log(`name: ${name}`);
+    console.log(`descriptor: ${descriptor}`);
+}
+function ParameterDecorator(target, propertyName, position) {
+    console.log('------ Parameter decorator --------');
+    console.log(`target: ${target}`);
+    console.log(`propertyName: ${propertyName}`);
+    console.log(`position: ${position}`);
+}
+class Product {
+    constructor(title, price) {
+        this.title = title;
+        this._price = price;
+    }
+    set price(value) {
+        if (value > 0) {
+            this._price = value;
+        }
+        else {
+            throw new Error('Invalid price: it should be positive number');
+        }
+    }
+    getPriceWithTax(taxRate) {
+        return this._price * (1 + taxRate);
+    }
+}
+__decorate([
+    PropertyDecorator
+], Product.prototype, "title", void 0);
+__decorate([
+    AccessorDecorator
+], Product.prototype, "price", null);
+__decorate([
+    MethodDecorator,
+    __param(0, ParameterDecorator)
+], Product.prototype, "getPriceWithTax", null);
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 /** @format */
+/*
 function Logger(randomString: string) {
   console.log('Logger factory');
   return function (constructor: Function) {
@@ -15,14 +16,19 @@ function WithTemplate(template: string, id: string) {
     const newPerson = new Person();
     if (appDiv) {
       appDiv.innerHTML = template;
-      appDiv.querySelector('h1')!.textContent = newPerson.name;
+      appDiv.querySelector('h1')!.textContent = newPerson.name
     }
-  };
+  }
 }
 
 /* ---------- Decorators ----------
+ *<Description>
  *Decorators provide a way to add both annotations and a meta-programming syntax for class *declarations and members
+ *<Timing of execution>
+ *It is executed when class exists, not when class initiated
  */
+
+/*
 @Logger('Logging')
 @WithTemplate('<h1>My Person Object</h1>', 'app')
 class Person {
@@ -35,3 +41,69 @@ class Person {
 
 const takuya = new Person();
 console.log(takuya);
+*/
+
+//  --------------- Accessor & Parameter Decorator -----------
+function PropertyDecorator(target: any, propertyName: string | Symbol) {
+  console.log('------ Property decorator --------');
+  console.log(`target: ${target}`);
+  console.log(`propertyName: ${propertyName}`);
+}
+
+function AccessorDecorator(
+  target: any,
+  name: string,
+  descriptor: PropertyDescriptor
+) {
+  console.log('------ Accessor decorator ------');
+  console.log(`target: ${target}`);
+  console.log(`name: ${name}`);
+  console.log(`descriptor: ${descriptor}`);
+}
+
+function MethodDecorator(
+  target: any,
+  name: string,
+  descriptor: PropertyDescriptor
+) {
+  console.log('------ Method decorator ------');
+  console.log(`target: ${target}`);
+  console.log(`name: ${name}`);
+  console.log(`descriptor: ${descriptor}`);
+}
+
+function ParameterDecorator(
+  target: any,
+  propertyName: string | Symbol,
+  position: number
+) {
+  console.log('------ Parameter decorator --------');
+  console.log(`target: ${target}`);
+  console.log(`propertyName: ${propertyName}`);
+  console.log(`position: ${position}`);
+}
+
+class Product {
+  @PropertyDecorator
+  title: string;
+  private _price: number;
+
+  constructor(title: string, price: number) {
+    this.title = title;
+    this._price = price;
+  }
+
+  @AccessorDecorator
+  set price(value: number) {
+    if (value > 0) {
+      this._price = value;
+    } else {
+      throw new Error('Invalid price: it should be positive number');
+    }
+  }
+
+  @MethodDecorator
+  getPriceWithTax(@ParameterDecorator taxRate: number) {
+    return this._price * (1 + taxRate);
+  }
+}
